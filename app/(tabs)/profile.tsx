a0a2b2737301a475430b915b68dc64469119234e
VolodymyrStreltsov <vladimir.streltsov@gmail.com>
2023-03-09 10:41:51 +0100

    profile removed sign-out

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { PageWrapper, Text } from '../../components'
-import { useAuth } from '../../context/auth/AuthProvider'
 
 export default function TabProfileScreen() {
-  const { signOut } = useAuth()
   return (
     <PageWrapper>
-      <Text onPress={() => signOut()}>Sign Out</Text>
+      <Text>Profile</Text>
     </PageWrapper>
   )
 }
